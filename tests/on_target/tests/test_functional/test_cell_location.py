c0377962e8502e9a15513f312903b6807dad3b93
Maximilian Deubel <maximilian.deubel@nordicsemi.no>
2025-11-18 15:47:41 +0100

    add remaining mqtt tests

--- tests/on_target/tests/test_functional/test_cell_location.py
+++ tests/on_target/tests/test_functional/test_cell_location.py
@@ -78,3 +78,37 @@ def test_rest_cell_location(dut_cloud, rest_cell_location_hex_file):
             break
     else:
         raise RuntimeError("No new locations observed")
+
+@pytest.mark.cell_location
+@pytest.mark.mqtt
+def test_mqtt_cell_location(dut_cloud, mqtt_cell_location_hex_file):
+    '''
+    Test that verifies that device can connect to nRF Cloud and request cell location.
+    '''
+
+    flash_device(os.path.abspath(mqtt_cell_location_hex_file))
+    dut_cloud.uart.xfactoryreset()
+    dut_cloud.uart.flush()
+
+    test_start_time = time.time()
+    reset_device()
+
+    dut_cloud.uart.wait_for_str_ordered(
+        [
+            "Connected to network",
+            "Current cell info: Cell ID: ",
+            "nrf_cloud_mqtt_cell_location: Lat:"
+        ],
+        timeout=CLOUD_TIMEOUT
+    )
+
+    # Poll for location to be reported to cloud
+    start = time.time()
+    while time.time() - start < CLOUD_TIMEOUT:
+        time.sleep(5)
+        locations = dut_cloud.cloud.get_location_history(dut_cloud.device_id, max_records=20, start=test_start_time)
+        logger.debug(f"Found locations: {locations}")
+        if len(locations) > 0:
+            break
+    else:
+        raise RuntimeError("No new locations observed")
