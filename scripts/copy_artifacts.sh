6d108a98ca92f9c4e63df595c1dc982399e4cfa4
Maximilian Deubel <maximilian.deubel@nordicsemi.no>
2026-06-10 14:01:55 +0200

    app_provision is not pre-merged.

--- scripts/copy_artifacts.sh
+++ scripts/copy_artifacts.sh
@@ -28,7 +28,7 @@ cp $BUILD_DIR/dfu_application.zip $TARGET_DIR
 cp $BUILD_DIR/dfu_mcuboot.zip $TARGET_DIR
 cp $BUILD_DIR/partitions.yml $TARGET_DIR
 cp $BUILD_DIR/build_info.yml $TARGET_DIR
-cp $BUILD_DIR/merged*.hex $TARGET_DIR
+cp $BUILD_DIR/*.hex $TARGET_DIR
 cp $BUILD_DIR/zephyr/.config $TARGET_DIR/zephyr-dotconfig.txt
 cp $BUILD_DIR/$SAMPLE_NAME/zephyr/.config $TARGET_DIR/app-dotconfig.txt
 cp $BUILD_DIR/$SAMPLE_NAME/zephyr/.config.sysbuild $TARGET_DIR/app-dotconfig.sysbuild.txt
